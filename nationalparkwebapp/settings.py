3df1d2dfeac18bf45828f90f69e7e6be06f98d21
xuro4 <49541578+xuro4@users.noreply.github.com>
2019-06-23 10:33:15 -0400

    Update settings.py

--- nationalparkwebapp/settings.py
+++ nationalparkwebapp/settings.py
@@ -159,8 +159,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Heroku DB Info Excluded
-DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+# DATABASES['default'] = dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 # DATABASES['default']['NAME'] = ''
 # DATABASES['default']['USER'] = ''
 # DATABASES['default']['PASSWORD'] = ''
