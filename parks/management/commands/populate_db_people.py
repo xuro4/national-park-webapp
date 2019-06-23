0d659a3b838a3701bab804163ac75b6d36288270
xuro4 <49541578+xuro4@users.noreply.github.com>
2019-06-23 12:37:56 -0400

    Update populate_db_people.py

--- parks/management/commands/populate_db_people.py
+++ parks/management/commands/populate_db_people.py
@@ -7,23 +7,26 @@ import logging
 
 
 class Command(BaseCommand):
-    help = 'Populates database with event information for each park'
+    help = 'Populates database with people information for each park'
 
     def pull_people_data(self):
         for park in Park.objects.all():
+            # API Settings
             endpoint = 'https://developer.nps.gov/api/v1/people'
             api_key = str(settings.API_KEY)
             params = {'api_key': api_key,
                       'parkCode': park.slug,
                       'limit': 50}
-
+            # Pull from API
             response = requests.get(endpoint, params=params).json()
             for entry in response['data']:
+                # Populate Database from Response JSON
                 title = entry['title']
                 img = entry['listingimage']['url']
                 desc = entry['listingdescription']
                 url = entry['url']
 
+                # If person isn't already in database, add them
                 if not People.objects.filter(title=title, park=park, img=img, desc=desc, url=url).exists():
                     new_person = People(title=title, park=park, img=img, desc=desc, url=url)
                     new_person.save()
