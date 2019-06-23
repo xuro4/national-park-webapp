141fd40484da0634f13328d6d1621f10fde68042
xuro4 <49541578+xuro4@users.noreply.github.com>
2019-06-23 12:25:34 -0400

    Added comments to population script

--- parks/management/commands/populate_db_alerts.py
+++ parks/management/commands/populate_db_alerts.py
@@ -7,22 +7,26 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
+    # Updated through Chron Job
     help = 'Populates database with event information for each park'
 
     def pull_alert_data(self):
         for park in Park.objects.all():
+            # API Settings
             endpoint = 'https://developer.nps.gov/api/v1/alerts'
             api_key = str(settings.API_KEY)
             params = {'api_key': api_key,
                       'parkCode': park.slug,
                       'limit': 50}
-
+            # Pull from API 
             response = requests.get(endpoint, params=params).json()
             for entry in response['data']:
+                # Populate information with returned JSON Data
                 title = entry['title']
                 desc = entry['description']
                 url = entry['url']
-
+                
+                # Save Alert into Database
                 new_alert = Alert(title=title, park=park, desc=desc, url=url)
                 new_alert.save()
 
