ff298c07fe536a7eaee7402ee84b555fb7801f92
xuro4 <49541578+xuro4@users.noreply.github.com>
2019-06-23 12:38:46 -0400

    Update populate_db_places.py

--- parks/management/commands/populate_db_places.py
+++ parks/management/commands/populate_db_places.py
@@ -7,23 +7,27 @@ import logging
 
 
 class Command(BaseCommand):
-    help = 'Populates database with event information for each park'
+    # Update using Cron Job
+    help = 'Populates database with place information for each park'
 
     def pull_places_data(self):
         for park in Park.objects.all():
+            # API Settings
             endpoint = 'https://developer.nps.gov/api/v1/places'
             api_key = str(settings.API_KEY)
             params = {'api_key': api_key,
                       'parkCode': park.slug,
                       'limit': 50}
-
+            # Pull from API
             response = requests.get(endpoint, params=params).json()
             for entry in response['data']:
+                # Populate data from Response JSON
                 title = entry['title']
                 img = entry['listingimage']['url']
                 desc = entry['listingdescription']
                 url = entry['url']
-
+                
+                # If place not already in database, add it
                 if not Places.objects.filter(title=title, park=park, img=img, desc=desc, url=url).exists():
                     new_place = Places(title=title, park=park, img=img, desc=desc, url=url)
                     new_place.save()
