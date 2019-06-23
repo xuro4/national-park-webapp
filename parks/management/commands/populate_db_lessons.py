41578de910530100e659da52b7c52385d15178ba
xuro4 <49541578+xuro4@users.noreply.github.com>
2019-06-23 12:33:52 -0400

    Update populate_db_lessons.py

--- parks/management/commands/populate_db_lessons.py
+++ parks/management/commands/populate_db_lessons.py
@@ -7,29 +7,35 @@ import logging
 
 
 class Command(BaseCommand):
+    # Update through Cron Job
     help = 'Populates database with event information for each park'
 
     def pull_lessons_data(self):
         for park in Park.objects.all():
+            # API Settings
             endpoint = 'https://developer.nps.gov/api/v1/lessonplans'
             api_key = str(settings.API_KEY)
             params = {'api_key': api_key,
                       'parkCode': park.slug,
                       'limit': 50}
-
+            # Pull from API
             response = requests.get(endpoint, params=params).json()
             for entry in response['data']:
+                # Populate Database from Response JSON
                 title = entry['title']
                 subject_raw = entry['subject']
+                # Add a space between commas for formatting
                 subject = subject_raw.replace(",", ", ")
-
+                
+                # Parse out the duration and remove unnecessary text
                 duration_str, space, minute = entry['duration'].partition(" ")
                 duration = int(duration_str)
 
                 desc = entry['questionobjective']
                 grade = entry['gradelevel']
                 url = entry['url']
-
+                
+                # if lesson not already in database, add it
                 if not LessonPlans.objects.filter(title=title, park=park, subject=subject, duration=duration,
                                                   grade_level=grade, url=url).exists():
                     new_lesson = LessonPlans(title=title, subject=subject, duration=duration, grade_level=grade,
